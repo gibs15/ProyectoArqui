dfc14aeaa2e928066ef7ee4f8dbe33365350261e
b58220 <b58220@ECCI-LBW-106-07>
2017-11-09 19:24:36 -0600

    Commit form

--- ProyectoArqui/Form1.Designer.cs
+++ ProyectoArqui/Form1.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.txtQuantum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtQuantum);
+            this.panel1.Controls.Add(this.btnIniciar);
+            this.panel1.Location = new System.Drawing.Point(544, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(271, 153);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(94, 101);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // txtQuantum
+            // 
+            this.txtQuantum.Location = new System.Drawing.Point(94, 57);
+            this.txtQuantum.Name = "txtQuantum";
+            this.txtQuantum.Size = new System.Drawing.Size(75, 20);
+            this.txtQuantum.TabIndex = 1;
+    
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Quantum:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SIMULADOR MIPS";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1389, 634);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQuantum;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ProyectoArqui/Form1.cs
+++ ProyectoArqui/Form1.cs
@@ -16,5 +16,35 @@ namespace ProyectoArqui
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            if (txtQuantum.Text != "")
+            {
+                try
+                {
+                    int ValorQuantum = Convert.ToInt32(txtQuantum.Text);
+                }
+                catch (Exception ex) {
+                    MessageBox.Show("El valor ingresado no fue un numero entero.");
+                    txtQuantum.Text = String.Empty;
+                }
+            }
+            else {
+                MessageBox.Show("Debe ingresar un valor para el Quantum.");
+            }
+        }
+
+        
     }
 }
